fe0d01b96c20291bdf8a2aba43626dbf8bedca7b
Colin Fendrick <colin.fendrick@gmail.com>
2020-11-26 05:45:15 -0800

    Add all the routes

---
+++ main.ts
@@ -0,0 +1,30 @@
+import { Application } from 'https://deno.land/x/abc@v1.2.2/mod.ts';
+import 'https://deno.land/x/dotenv/load.ts';
+
+import {
+	getAllContact,
+	createContact,
+	getOneContact,
+	updateContact,
+	deleteContact
+} from './controllers/contacts.ts';
+
+import {
+	ErrorMiddleware,
+	LogMiddleware
+} from './utils/middleware.ts';
+
+const app = new Application();
+app
+	.use(LogMiddleware)
+	.use(ErrorMiddleware);
+
+app
+	.get('/contacts', getAllContact)
+	.post('/contact', createContact)
+	.get('/contact/:id', getOneContact)
+	.put('/contact/:id', updateContact)
+	.delete('/contact/:id', deleteContact)
+	.start({ port: 5000 });
+
+console.log('server listening on http://localhost:5000');
